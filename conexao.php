ff5e961a45cebc6240db9ac873784e80103aa064
Queijoz <lqggamer@outlook.com>
2021-03-01 15:58:07 -0300

    Add Basic SQL Commands + Debug Printing PHP Structure

--- conexao.php
+++ conexao.php
@@ -4,10 +4,32 @@ error_reporting(E_ERROR | E_PARSE);
 $servidor = "localhost";
 $usuario = "root";
 $senha = "";
+$database = "buscarCep";
 
-$conexao = new mysqli($servidor, $usuario, $senha);
+$conexao = new mysqli($servidor, $usuario, $senha, $database);
 
+// Trata o erro, caso não seja possível acessar o banco de dados.
 if ($conexao->connect_error){
     echo '<script>alert("Não foi possível se conectar ao banco de dados, devido ao erro:\n'.$conexao->connect_error.'")</script>';
+}
+
+
+function getConsulta($conexao, $cep){
+    // Checa se a consulta já existe.
+    $sqlQuery = "SELECT * FROM consulta WHERE cep='".$cep."'";
+    $resultado = $conexao->query($sqlQuery);
+
+    if($resultado->num_rows != 0){ 
+        // Caso exista, pega a consulta do bando de dados em forma de objeto.
+        $consulta = (object) $resultado->fetch_assoc();
+        return $consulta;
+    }
+}
+
+// Função usada para debugs pelo console do navegador.
+function consoleLog($data){
+    echo '<script>
+            console.log("'.$data.'")
+        </script>';
 }
 ?>
--- index.php
+++ index.php
@@ -18,24 +18,39 @@
         }
     </style>
 
+    <?php // Start Connection with Database
+    require 'conexao.php';?>
+
   </head>
 
   <body class="d-flex align-items-center">
     <?php
       // Checa se a url possui o parâmetro do CEP, para mostrar as informações do mesmo.
-      if($_SERVER['REQUEST_METHOD'] == "GET" and isset($_GET['cep'])){
-
-        // Pega a URL sem parâmetros para o botão de Nova Consulta.
-        $uri_parts = explode('?', $_SERVER['REQUEST_URI'], 2);
-        $url = 'http://' . $_SERVER['HTTP_HOST'] . $uri_parts[0];
+      if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['cep'])){
+        
+        // Verifica se o CEP informado existe no banco de dados.
+        $consulta = getConsulta($conexao, $_POST['cep']);
+        if (!$consulta){
+          // Caso não exista, pega as informações do site ViaCep.
+          $response = file_get_contents('https://viacep.com.br/ws/'.$_POST['cep'].'/xml');
+          $consulta = simplexml_load_string($response);
+        }
 
         // Exibe todas as informações encontradas do CEP informado.
         echo '<div class="container bg-light p-5 text-center">
-              <h1 class="display-4">Cidade/Estado</h1>
-              <p class="lead">Bairro.</p>
-              <p class="lead">DDD.</p>
-              <p class="lead">Logradouro.</p>
-              <a class="btn btn-primary btn-lg rounded-0 mt-3" href="'.$url.'">Nova Consulta</a></div>';
+        <h1 class="display-4 mb-5">'.$consulta->id.' / '.$consulta->cep.'</h1>';
+
+        /*if($consulta->bairro != ""){echo '<p class="lead">Bairro: '.$consulta->bairro.'</p>';}
+        if($consulta->logradouro != ""){echo '<p class="lead">Logradouro: '.$consulta->logradouro.'</p>';}
+        if($consulta->complemento != ""){echo '<p class="lead">Complemento: '.$consulta->complemento.'</p>';}
+        if($consulta->ddd != ""){echo '<p class="lead">DDD: '.$consulta->ddd.'</p>';}
+        if($consulta->ibge != ""){echo '<p class="lead">IBGE: '.$consulta->ibge.'</p>';}
+        if($consulta->siafi != ""){echo '<p class="lead">SIAFI: '.$consulta->siafi.'</p>';}*/
+
+        // Extrai o CEP da URL.
+        $uri_parts = explode('?', $_SERVER['REQUEST_URI'], 2);
+        $url = 'http://' . $_SERVER['HTTP_HOST'] . $uri_parts[0];
+        echo '<a class="btn btn-primary btn-lg rounded-0 mt-3" href="'.$url.'">Nova Consulta</a></div>';
       }else{
         // Caso a URL não possua o parâmetro do CEP, exibe o formulário de consulta.
         echo 
@@ -43,7 +58,7 @@
             <h1 class="display-4">Buscar Endereço</h1>
             <p class="lead">Procure um endereço pelo CEP.</p>
       
-            <form class="input-group mt-5 justify-content-center" method="get">
+            <form class="input-group mt-5 justify-content-center" method="post">
               <input type="text" class="form-control rounded-0" style="max-width: 400" placeholder="CEP" name="cep">
               <input class="btn btn-primary btn-lg rounded-0" id="buttonBuscar" type="submit" value="Buscar">
             </form>
